8584446f2c85ac0b02ba1ece23d5b43aa9cdeb6d
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2020-12-14 01:11:08 +0530

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,105 +1,57 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-const marvelDB = {
+var marvelDB = {
   spiderman: [
     {
+      id: 1,
+      director: "sam raimi",
       name: "Spider-Man",
       release: "2002",
       rating: "7.3/10",
       description:
         "Spider-Man is a 2002 American superhero film based on the Marvel Comics character of the same name"
-    },
-    {
-      name: "Spider-Man2",
-      release: "2004",
-      rating: "7.3/10",
-      description:
-        "Spider-man2 is 2004 movie based on spiderman and doctor octivious"
-    },
-
-    {
-      name: "Spider-Man3",
-      release: "2007",
-      rating: "7.3/10",
-      description: "spiderman3 is 2007 movie based on spiderman and venom"
     }
   ],
 
   ironman: [
     {
-      name: "ironman",
+      id: 2,
+      director: "Jon Favreau",
+      name: "Ironman",
       release: "2008",
       rating: "8.7/10",
       description:
         "Based on popular marvel character ironman played by robert downeyjr"
-    },
-
-    {
-      name: "ironman2",
-      release: "2010",
-      rating: "8.9/10",
-      description: "second part of ironman franchaise"
-    },
-    {
-      name: "ironman3",
-      release: "2013",
-      rating: "9.0/10",
-      description: "last part of ironman franchaise"
     }
   ],
 
   avengers: [
     {
-      name: "avengers",
-      release: "2012",
+      id: 3,
+      director: "joss wheedon",
+      name: "Avengers",
+      release: "2018",
       rating: "8.7/10",
       description:
         "first part of avenger franchaise main marvel characters team up to form avengers team against loki "
-    },
-
-    {
-      name: "avengers age of ultron",
-      release: "2015",
-      rating: "9.2/10",
-      description: "second part of avenger franchaise entry of vision wanda"
-    },
-
-    {
-      name: "avengers infinity war",
-      release: "2018",
-      rating: "8.1/10",
-      description:
-        "third part of avenger franchaise everyone team up against thanos"
     }
   ],
 
   captainamerica: [
     {
-      name: "captain america first avenger",
+      id: 4,
+      director: "joe joshton",
+      name: "Captain america first avenger",
       release: "2011",
       rating: "8.6/10",
       description: "first part of captain america series"
-    },
-
-    {
-      name: "captain america winter solidier",
-      release: "2014",
-      rating: "8.8/10",
-      description: "second part of captain america bucky as a rival"
-    },
-
-    {
-      name: "captain america civil war",
-      release: "2016",
-      rating: "7.4/10",
-      description: "civil war rivalry between captain america and ironman"
     }
   ]
 };
 
 export default function App() {
-  const [selectMovie, setSelectMovie] = useState("ironman");
+  var [selectMovie, setSelectMovie] = useState("ironman");
 
   function movieClickHandler(movie) {
     setSelectMovie(movie);
@@ -108,7 +60,6 @@ export default function App() {
     <div className="App">
       <h1>Marvel-Movies-Recommender</h1>
       <h2>Watch this awesome marvel movies.</h2>
-
       {Object.keys(marvelDB).map((movie) => (
         <button onClick={() => movieClickHandler(movie)}>{movie}</button>
       ))}
@@ -116,6 +67,7 @@ export default function App() {
       <ul>
         {marvelDB[selectMovie].map((category) => (
           <li key={category.id}>
+            {/* <div>{category.id}</div> */}
             <div>{category.name}</div>
             <div>{category.release}</div>
             <div>{category.rating}</div>
